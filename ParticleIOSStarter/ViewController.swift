9737cc61253a896c2b9ea0ac6ea951d9fc4cf1f3
kbkakhil <kbkakhil459@gmail.com>
2019-11-07 11:46:55 -0500

    added timer to the phone

--- ParticleIOSStarter/ViewController.swift
+++ ParticleIOSStarter/ViewController.swift
@@ -20,12 +20,57 @@ class ViewController: UIViewController {
     var myPhoton : ParticleDevice?
 
   
-
+    @IBOutlet weak var timerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ParticleCloud.init()
+        ParticleCloud.sharedInstance().login(withUser: self.USERNAME, password: self.PASSWORD){ (error:Error?) -> Void in
+            if (error != nil) {
+                // Something went wrong!
+                print("Wrong credentials or as! ParticleCompletionBlock no internet connectivity, please try again")
+                // Print out more detailed information
+                print(error?.localizedDescription)
+            }
+            else {
+                print("Login success!")
+                
+                // try to get the device
+                self.getDeviceFromCloud()
+                
+            }
+        }
         
       
     }
     
+    func getDeviceFromCloud() {
+        ParticleCloud.sharedInstance().getDevice(self.DEVICE_ID) { (device:ParticleDevice?, error:Error?) in
+            
+            if (error != nil) {
+                print("Could not get device")
+                print(error?.localizedDescription)
+                return
+            }
+            else {
+                print("Got photon from cloud: \(device?.id)")
+                self.myPhoton = device
+                
+            }
+            
+        } 
+    }
+    var timer = Timer()
+    var time = 0
+    
+    @IBAction func startButton(_ sender: Any) {
+
+       timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true)
+    }
+    
+    @objc func action() {
+        time += 1
+        timerLabel.text = String(time)
+    }
 }
 
