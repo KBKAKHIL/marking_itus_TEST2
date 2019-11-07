bd68481c519b8467cb9790794dcf78612e80311f
kbkakhil <kbkakhil459@gmail.com>
2019-11-07 10:59:09 -0500

    Initial Commit

---
+++ ParticleIOSStarter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ParticleIOSStarter
+//
+//  Created by Parrot on 2019-06-29.
+//  Copyright © 2019 Parrot. All rights reserved.
+
+import UIKit
+import Particle_SDK
+
+class ViewController: UIViewController {
+
+    // MARK: User variables
+    let USERNAME = "kbkakhil459@gmail.com"
+    let PASSWORD = "Kennyakhil27"
+    
+    // MARK: Device
+    
+    let DEVICE_ID = "3b0021000247363333343435"
+    var myPhoton : ParticleDevice?
+
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+      
+    }
+    
+}
+
